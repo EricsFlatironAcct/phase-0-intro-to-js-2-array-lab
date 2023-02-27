e6c4f2cd2ce2198b1a95713ca824eda0367922f8
Eric Krukowski <erickruk@amazon.com>
2023-02-27 05:53:23 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
+//adds to end
 function destructivelyAppendCat(name){
     cats.push(name);
 }
+//adds to beginning
 function destructivelyPrependCat(name){
     cats.unshift(name);
 }
+//removes from beginning
 function destructivelyRemoveFirstCat(){
     cats.shift();
 }
+//removes from end
 function destructivelyRemoveLastCat(){
     cats.pop();
 }
+//returns new object with object added to end
 function appendCat(name){
     return [...cats, name];
 }
+//returns new object with object added to beginning
 function prependCat(name){
     return[name, ...cats];
 }
+//returns new object with last item removed
 function removeLastCat(){
     return cats.slice(0,cats.length-1);
 }
+//returns new object with first item removed
 function removeFirstCat(){
     return cats.slice(1);
 }
